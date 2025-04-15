5cfdb933c1e461663dfe7546effb52c37fee480f
배준하 <bae01028755124@gmail.com>
2025-04-15 19:06:43 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,23 @@
+a = int(input())
+if a == 1:
+    print(12,1600)
+elif a== 2:
+    print(11,894)
+elif a== 3:
+    print(11,1327)
+elif a== 4:
+    print(10,1311)
+elif a== 5:
+    print(9,1004)
+elif a== 6:
+    print(9,1178)
+elif a== 7:
+    print(9,1357)
+elif a== 8:
+    print(8,837)
+elif a== 9:
+    print(7,1055)
+elif a== 10:
+    print(6,556)
+elif a== 11:
+    print(6,773)
