c9a05e4063a24c8e59c4538457db19c8265998fa
배준하 <bae01028755124@gmail.com>
2025-04-15 20:37:21 +0900

    [Bronze V] Title: 피갤컵, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a == 1:
+    print("2024 8")
+elif a== 3:
+    print("2025 10")
+elif a== 2:
+    print('2025 3')
+elif a == 4:
+    print("2026 5")
+else:
+    print("2026 12")
