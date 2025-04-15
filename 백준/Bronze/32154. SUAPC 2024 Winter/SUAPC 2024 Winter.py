eaa1ae50121f44fa1c788b6b7d93ea5e094c830b
배준하 <bae01028755124@gmail.com>
2025-04-15 19:59:28 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,31 @@
+a = int(input())
+if a == 1:
+    print(11)
+    print("A B C D E F G H J L M")
+elif a == 2:
+    print(9)
+    print("A C E F G H I L M")
+elif a == 3:
+    print(9)
+    print("A C E F G H I L M")
+elif a == 4:
+    print(9)
+    print("A B C E F G H L M")
+elif a == 5:
+    print(8)
+    print("A C E F G H L M")
+elif a == 6:
+    print(8)
+    print("A C E F G H L M")
+elif a == 7:
+    print(8)
+    print("A C E F G H L M")
+elif a == 8:
+    print(8)
+    print("A C E F G H L M")
+elif a == 9:
+    print(8)
+    print("A C E F G H L M")
+elif a == 10:
+    print(8)
+    print("A B C F G H L M")
