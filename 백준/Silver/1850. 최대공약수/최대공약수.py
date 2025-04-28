34002c57c50c5199b4b45cf66a612bf315a261c4
배준하 <bae01028755124@gmail.com>
2025-04-28 17:12:52 +0900

    [Silver I] Title: 최대공약수, Time: 44 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Silver/1850. 최대공약수/최대공약수.py
+++ 백준/Silver/1850. 최대공약수/최대공약수.py
@@ -1,8 +1,7 @@
-import sys
 def gcd(a,b):
     if b == 0:
         return a
     else:
         return gcd(b,a%b)
-a,b = map(int, sys.stdin.readline().split())
+a,b = map(int, input().split())
 print("1"*gcd(a,b))
