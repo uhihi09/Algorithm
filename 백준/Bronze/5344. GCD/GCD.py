6e1566bf150b6a3dc1e0fcfff01dc9b8300aa175
배준하 <bae01028755124@gmail.com>
2025-04-28 16:46:06 +0900

    [Bronze I] Title: GCD, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5344. GCD/GCD.py
@@ -0,0 +1,10 @@
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b,a%b)
+
+a = int(input())
+for i in range(a):
+    b,c = map(int,input().split())
+    print(gcd(b,c))
