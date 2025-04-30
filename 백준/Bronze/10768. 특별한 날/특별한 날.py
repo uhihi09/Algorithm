2a109a48d60c0f0944344c1cfd1e1bdb71e3ff0d
배준하 <bae01028755124@gmail.com>
2025-04-30 14:39:32 +0900

    [Bronze IV] Title: 특별한 날, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+if a == 2 and b == 18:
+    print("Special")
+elif a == 2 and b < 18:
+    print("Before")
+elif a == 2 and b > 18:
+    print("After")
+elif a < 2:
+    print("Before")
+else:
+    print("After")
