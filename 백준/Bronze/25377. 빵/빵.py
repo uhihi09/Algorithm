116f40f064d9027641ddf0b8f608eb7f5220e0cc
배준하 <bae01028755124@gmail.com>
2025-05-09 22:47:34 +0900

    [Bronze IV] Title: 빵, Time: 36 ms, Memory: 32544 KB, Score: 68 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,20 +1,13 @@
 a = int(input())
-cnt = 0
-ans = 0
-ans1 = True
-ans2 = 0
+cnt1 = 0
+cnt2 = 0
+d = []
 for i in range(a):
-    b,c = map(int, input().split())
-    cnt += b
-    ans += c
-    if ans < cnt:
-        ans1 = False
-    elif ans >= cnt:
-        if c-b >= 0:
-            ans2 += (c - b)
-        else:
-            ans2 += b-c
-if ans1 == False:
-    print(-1)
-else:
-    print(ans2)
+    b,c = map(int,input().split())
+    cnt1 += b
+    cnt2 += c
+    if c >= b:
+        d.append(c)
+    elif cnt2 < cnt1:
+        d.append(-1)
+print(min(d))
