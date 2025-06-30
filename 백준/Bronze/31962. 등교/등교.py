a5f04a1010a58c83fc4a74cfcf834622b462a271
배준하 <junhabae0729@gmail.com>
2025-06-30 12:28:31 +0900

    [Bronze IV] Title: 등교, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31962. 등교/등교.py
+++ 백준/Bronze/31962. 등교/등교.py
@@ -1,14 +1,12 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
-cnt = c+d
 if c+d <= b:
-    cnt1 = c
+    cnt = c
 else:
-    cnt1 = -1
+    cnt = -1
 for i in range(a-1):
     c,d = map(int,input().split())
     if c+d <= b:
-        if c+d > cnt:
-            cnt = c+d
-            cnt1 = c
-print(cnt1)
+        if cnt < c:
+            cnt = c
+print(cnt)
