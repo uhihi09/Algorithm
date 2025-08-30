871ab9695be988c0688e0cdf3bfdaca1030b66bf
배준하 <junhabae0729@gmail.com>
2025-08-30 23:55:33 +0900

    [Bronze IV] Title: Yalda, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a >= b:
+    print("Watermelon")
+elif a >= c:
+    print("Pomegranates")
+elif a >= d:
+    print("Nuts")
+else:
+    print("Nothing")
