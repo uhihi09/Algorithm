da9a458d9d19ad67b8b516c9a5eed1729d4b00d1
배준하 <junhabae0729@gmail.com>
2025-09-06 00:03:13 +0900

    [Bronze IV] Title: A + B Queries, Time: 36 ms, Memory: 9080 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34210. A ＋ B Queries/A ＋ B Queries.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include "aplusb.h"
+using namespace std;
+ 
+vector<int> a, b;
+ 
+void initialize(vector<int> A, vector<int> B) {
+    a = A;
+    b = B;
+    return;
+}
+ 
+int answer_question(int i, int j) {
+    return a[i] + b[j];
+}
