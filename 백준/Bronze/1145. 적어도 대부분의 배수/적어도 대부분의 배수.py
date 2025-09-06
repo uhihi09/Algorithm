82188b2501ebcb7719232dce4b86c2350dcf91b2
배준하 <junhabae0729@gmail.com>
2025-09-06 23:57:31 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 488 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+a = list(map(int,input().split()))
+ans = min(a)
+while True:
+    cnt = 0
+    for i in a:
+        if ans % i == 0:
+            cnt += 1
+    if cnt > 2:
+        break
+    ans += 1
+print(ans)
