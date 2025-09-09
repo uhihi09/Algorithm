9ef42e0d299e126d39e3ebface404c25d59895b1
배준하 <junhabae0729@gmail.com>
2025-09-09 17:24:16 +0900

    [Bronze IV] Title: Hurra!, Time: 668 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/26767. Hurra！/Hurra！.py
@@ -0,0 +1,10 @@
+a = int(input())
+for i in range(1,a+1):
+    if i % 7 == 0 and i % 11 != 0 :
+        print("Hurra!")
+    elif i % 7 != 0 and i % 11 == 0:
+        print("Super!")
+    elif i % 7 == 0 and i % 11 == 0:
+        print("Wiwat!")
+    else:
+        print(i)
