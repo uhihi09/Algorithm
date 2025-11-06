81045ed76056477ffd540a284a7b2c2d8824c19f
배준하 <junhabae0729@gmail.com>
2025-11-06 10:11:49 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,6 @@
+a = int(input())
+for i in range(1,a+1):
+    b = list(map(int,input().split()))
+for i in range(1,a+1):
+    print(f"Material Management {i}")
+    print("Classification ---- End!")
