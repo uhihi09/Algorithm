3aa7131afb5115e0d356a335720b10e7a1d54401
배준하 <junhabae0729@gmail.com>
2025-12-07 00:35:03 +0900

    [Bronze I] Title: A/B - 2, Time: 44 ms, Memory: 32544 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,4 +1,15 @@
 import sys
 input = sys.stdin.readline
-a,b = map(int,input().split())
-print(a/b)
+a, b = map(int, input().split())
+result = [str(a // b)]
+a %= b
+if a:
+    result.append(".")
+    for _ in range(1000):
+        a *= 10
+        result.append(str(a // b))
+        a %= b
+        if a == 0:
+            break
+
+print("".join(result))
