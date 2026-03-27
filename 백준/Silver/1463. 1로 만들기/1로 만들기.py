0ee0e29c93952524e5c1049799569d6d6d5c536a
Baejunha <junhabae0729@gmail.com>
2026-03-27 10:28:02 +0900

    [Silver III] Title: 1로 만들기, Time: 64 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,18 @@
+import collections
 a = int(input())
-dp = [0] * (a + 1)
-dp[1] = 0
-
-for i in range(2, a + 1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-
-print(dp[a])
+q = collections.deque()
+q.append((a,0))
+visited = set()
+while q:
+    num,cnt = q.popleft()
+    if num in visited:
+        continue
+    visited.add(num)
+    if num == 1:
+        print(cnt)
+        break
+    if num % 3 == 0:
+        q.append((num//3, cnt + 1))
+    if num % 2 == 0:
+        q.append((num//2, cnt + 1))
+    q.append((num-1, cnt + 1))
