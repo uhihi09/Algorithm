981e7d48e31368617fd771c7b752e70bf89160e6
Junha Bae <junhabae0729@gmail.com>
2026-05-19 14:56:23 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.59 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr;
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (j % queries[i][2] == 0) {
+                    answer[j] += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
